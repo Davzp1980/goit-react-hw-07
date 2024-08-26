612f3dafd45412cb76fcb2ff5f97cc4be2273ebf
Oleksii Donets <108811517+Davzp1980@users.noreply.github.com>
2024-08-26 13:44:36 +0300

    done

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -37,12 +37,13 @@ const contactsSlice = createSlice({
 
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = true;
+        state.isLoading = false;
         state.error = null;
 
         const index = state.items.findIndex(
-          contact => contact.id === action.payload
+          contact => contact.id === action.payload.id
         );
+
         state.items.splice(index, 1);
       })
       .addCase(deleteContact.rejected, handleRejected);
